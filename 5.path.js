ea024bf8b931e5f3a96696814d924834ef08e20c
Jairdevv <145697757+Jairdevv@users.noreply.github.com>
2024-01-22 12:12:45 -0500

    node curse advancement

---
+++ 5.path.js
@@ -0,0 +1,17 @@
+const path = require('node:path')
+
+// Barra separadora de carpetas
+console.log(path.sep)
+
+// unir rutas con path.join
+const filePath = path.join('content', 'subfolder', 'test.txt')
+console.log(filePath)
+
+const base = path.basename('/tmp/jair-secret-files/password.txt')
+console.log(base)
+
+const filename = path.basename('/tmp/jair-secret-files/password.txt', '.txt')
+console.log(filename)
+
+const extraname = path.extname('/tmp/jair-secret-files/password.txt')
+console.log(extraname)
